0d70024b66c6357a561f21c0a18e3e4c644d63c6
Mellonka <facemellon@gmail.com>
2024-05-14 23:59:05 +0500

    feat: add styles

--- app/recipes/view_routers.py
+++ app/recipes/view_routers.py
@@ -1,10 +1,9 @@
-from uuid import UUID
-
-from fastapi import APIRouter, Depends, Query, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 
-from app.core.database import AsyncSession, async_db_session
 from app.recipes.api_routers import api_get_recipe, api_get_recipes
+
+from app.core.models.recipe import RecipeSchema
 
 view_recipes_router = APIRouter()
 
@@ -14,22 +13,8 @@ templates = Jinja2Templates(directory="app/templates")
 @view_recipes_router.get("/")
 async def view_index(
     request: Request,
-    offset: int = Query(0, ge=0),
-    limit: int = Query(100, ge=1, le=100),
-    name: str | None = Query(None),
-    tags: list[str] | None = Query(None),
-    ingredients: list[str] | None = Query(None),
-    db_session: AsyncSession = Depends(async_db_session),
+    recipes: list[RecipeSchema] = Depends(api_get_recipes),
 ):
-    recipes = await api_get_recipes(
-        offset=offset,
-        limit=limit,
-        name=name,
-        tags=tags,
-        ingredients=ingredients,
-        db_session=db_session,
-    )
-
     return templates.TemplateResponse(
         request=request,
         name="index.html",
@@ -42,11 +27,8 @@ async def view_index(
 @view_recipes_router.get("/recipes/{recipe_id}")
 async def view_recipe(
     request: Request,
-    recipe_id: UUID,
-    db_session: AsyncSession = Depends(async_db_session),
+    recipe: RecipeSchema = Depends(api_get_recipe),
 ):
-    recipe = await api_get_recipe(recipe_id=recipe_id, db_session=db_session)
-
     return templates.TemplateResponse(
         request=request,
         name="recipe.html",
